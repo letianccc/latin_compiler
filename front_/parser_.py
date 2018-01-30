6956dcf0ba6ae7c95059bc8f9b2baec1d174e122
latin <704364447@qq.com>
2018-01-31 03:26:18 +0800

    s

--- front_/parser_.py
+++ front_/parser_.py
@@ -20,24 +20,33 @@ class Parser:
 
     def next_token(self):
         t = self.tokens[self.index]
-        self.index_forward()
+        self.increase_index()
         return t
 
     def last_token(self):
         return self.tokens[self.index-1]
 
-    def index_forward(self):
+    def increase_index(self):
         self.index += 1
 
     def return_token(self):
         self.index += 1
 
     def match(self, word):
-        name = self.next_token().name
-        if name != word:
-            print('not match!',
-            'last:  ', self.tokens[self.index-2].name)
-            print(self.index-1, name, word)
+        if self.index < len(self.tokens):
+            name = self.cur_token().name
+            if name != word:
+                print('not match!')
+                print('last:  ', self.tokens[self.index-2].name, self.tokens[self.index-1].name)
+                print('index: ', self.index, 'len:  ', len(self.tokens))
+                print('match:  ', name)
+                print('expect: ', word)
+                raise Exception
+            self.increase_index()
+        else:
+            print('index out of range!')
+            print('last:  ', self.tokens[self.index-1].name)
+            print('expect: ', word)
             raise Exception
 
     def block_(self):
@@ -63,21 +72,36 @@ class Parser:
         t = self.cur_token()
         name = t.name
         if self.is_word('if'):
-            self.match('if')
-            self.match('(')
-            cond = self.bool_()
-            self.match(')')
-            then_stmts = self.block_()
-            if self.is_word('else'):
-                self.match('else')
-                else_stmts = self.block_()
-            else:
-                else_stmts = None
-            return If(cond, then_stmts, else_stmts)
+            return self.if_stmt()
         elif self.is_word('int') or self.is_word('float'):
             return self.decl()
+        elif self.is_word('while'):
+            return self.while_stmt()
+        elif self.is_word('{'):
+            return self.block_()
         else:
             return self.assign()
+
+    def while_stmt(self):
+        self.match('while')
+        self.match('(')
+        cond = self.bool_()
+        self.match(')')
+        suite = self.block_()
+        return While(cond, suite)
+
+    def if_stmt(self):
+        self.match('if')
+        self.match('(')
+        cond = self.bool_()
+        self.match(')')
+        then_stmts = self.block_()
+        if self.is_word('else'):
+            self.match('else')
+            else_stmts = self.block_()
+        else:
+            else_stmts = None
+        return If(cond, then_stmts, else_stmts)
 
     def decl(self):
         type_ = self.next_token().name
@@ -88,7 +112,7 @@ class Parser:
     def assign(self):
         variable = self.next_token()
         self.match('=')
-        value = self.expr_()
+        value = self.bool_()
         self.match(';')
         return Assign(variable, value)
 
@@ -96,14 +120,14 @@ class Parser:
         expr = self.join()
         while self.is_word('||'):
             self.match('||')
-            expr = Or(expr, self.bool_())
+            expr = Or(expr, self.join())
         return expr
 
     def join(self):
         expr = self.equal()
         while self.is_word('&&'):
             self.match('&&')
-            expr = And(expr, self.join())
+            expr = And(expr, self.equal())
         return expr
 
     def equal(self):
@@ -111,12 +135,11 @@ class Parser:
 
         while self.is_word('==') or self.is_word('!='):
             operator = self.next_token().name
-            expr = Equal(expr, self.equal(), operator)
+            expr = Equal(expr, self.rel(), operator)
         return expr
 
     def rel(self):
         expr = self.expr_()
-
         while self.is_word('<') or self.is_word('<=') or self.is_word('>') or self.is_word('>='):
             operator = self.next_token().name
             expr = Rel(expr, self.expr_(), operator)
